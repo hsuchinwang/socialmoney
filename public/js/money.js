677c1f782b873f81a66bce9843cb153d03889b57
hsuchinwang <sam30278@gmail.com>
2016-05-05 09:08:41 +0800

    fix security problem:conbine method

--- public/js/money.js
+++ public/js/money.js
@@ -176,35 +176,52 @@ function removeImg() {
 
 
 
-function updateDB(resultText) {
-
+function updateDB() {
+    var pin = $('#pin').val;
+    var resultText = '';
     // var resultText = $("#result").text();
-    resultText = resultText.replace(/幣/g,"");
-    resultText = resultText.replace(/元/g,"");
-    resultText = resultText.replace(/\./g,"");
-    var tmp = resultText.split(',');
-    var resultPri = tmp[2];
-    var resultName = tmp[0];
-    var resultNum = tmp[1].split(/\D+/);
-    var resultCur = tmp[1].split(/\d+/);
-    var creditname = $("#name").html().toString();
-    resultNum.shift();
-    resultCur.pop();
-    console.log(tmp,resultPri,resultName,resultCur,resultNum, creditname);
-    $.post('https://socialmoney.herokuapp.com/minus', {name: resultName, currency: resultCur, price: resultNum }, function(result) {
-
-      console.log(result);
-
+    $.post('https://socialmoney.herokuapp.com/getpin', { pin: pin }, function(result) {
+        if (result.toString() != 'fail'){
+          resultText = result.toString();
+          alert('The record is saved!');
+        } else {
+          alert('The Pin Code is wrong! Please input the correct one!');
+        }
+        
     });
-    $.post('https://socialmoney.herokuapp.com/add', {name: creditname, currency: resultCur, price: resultNum}, function(result) {
+    if (resultText != '') {
+        //updateDB(resultText);
+        resultText = resultText.replace(/幣/g,"");
+        resultText = resultText.replace(/元/g,"");
+        resultText = resultText.replace(/\./g,"");
+        var tmp = resultText.split(',');
+        var resultPri = tmp[2];
+        var resultName = tmp[0];
+        var resultNum = tmp[1].split(/\D+/);
+        var resultCur = tmp[1].split(/\d+/);
+        var creditname = $("#name").html().toString();
+        resultNum.shift();
+        resultCur.pop();
+        console.log(tmp,resultPri,resultName,resultCur,resultNum, creditname);
+        $.post('https://socialmoney.herokuapp.com/minus', {name: resultName, currency: resultCur, price: resultNum }, function(result) {
 
-      console.log(result);
+          console.log(result);
 
-    });
-    $.post('https://socialmoney.herokuapp.com/keeprecord', {namec: creditname, named: resultName, price: resultPri, currency: tmp[1].toString()}, function(result) {
+        });
+        $.post('https://socialmoney.herokuapp.com/add', {name: creditname, currency: resultCur, price: resultNum}, function(result) {
 
-      console.log(result);
+          console.log(result);
 
-    });
+        });
+        $.post('https://socialmoney.herokuapp.com/keeprecord', {namec: creditname, named: resultName, price: resultPri, currency: tmp[1].toString()}, function(result) {
+
+          console.log(result);
+
+        });
+      }
+      $('#pin').val = '';
+
+    }
+    
 
 }
