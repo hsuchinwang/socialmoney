0604df344281c2f879190b9f77bf944d10829376
hsuchinwang <sam30278@gmail.com>
2016-05-10 21:37:06 +0800

    success for fixing ChiOS problem. clean redundant element.

--- public/js/postfb.js
+++ public/js/postfb.js
@@ -6,18 +6,18 @@ function statusChangeCallback(response) {
       // Logged into your app and Facebook.
       //window.location.replace("/success");
       getUserPic();
-      document.getElementById("logindiv").style.display = "none";
-      document.getElementById('status').innerHTML = "";
+      // document.getElementById("logindiv").style.display = "none";
+      // document.getElementById('status').innerHTML = "";
     } else if (response.status === 'not_authorized') {
       // The person is logged into Facebook, but not your app.
-      document.getElementById('status').innerHTML = 'Please log ' +
-        'into this app.';
+      //document.getElementById('status').innerHTML = 'Please log ' +
+      //  'into this app.';
         loginWithFacebook();
     } else {
       // The person is not logged into Facebook, so we're not sure if
       // they are logged into this app or not.
-      document.getElementById('status').innerHTML = 'Please log ' +
-        'into Facebook.';
+      //document.getElementById('status').innerHTML = 'Please log ' +
+      //  'into Facebook.';
         loginWithFacebook();
     }
   }
